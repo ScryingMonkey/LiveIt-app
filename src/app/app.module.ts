2514335a9600a6ae79a42e4b22118e380a17419f
Luke <cliftonlboyd@gmail.com>
2017-03-21 00:48:04 -0400

    setup firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { AngularFireModule } from 'angularfire2';
 
 import { AppComponent } from './app.component';
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyB3uJ_NcsO30q-QX6FYvCKh9f9p7K8JfiI",
+    authDomain: "liveit-app.firebaseapp.com",
+    databaseURL: "https://liveit-app.firebaseio.com",
+    storageBucket: "liveit-app.appspot.com",
+    messagingSenderId: "712223429525"
+}
 
 @NgModule({
   declarations: [
@@ -12,9 +21,12 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
-  providers: [],
+  providers: [
+
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
